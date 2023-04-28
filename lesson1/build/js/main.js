f0b767ca5998222e75836ded84b1aee5bf49cf3e
LilJho <jhonnelg25@gmail.com>
2023-04-28 23:00:09 +0800

    lesson1 done

---
+++ lesson1/build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let username = "Jhonnel";
+let a = 12;
+let b = "6";
+let c = 2;
+console.log(username);
+console.log(a / b);
+console.log(b * c);
---
+++ lesson1/src/main.ts
@@ -0,0 +1,9 @@
+let username = "Jhonnel";
+let a: number = 12;
+let b: string = "6";
+let c: number = 2;
+
+console.log(username);
+
+console.log(a / b);
+console.log(b * c);
